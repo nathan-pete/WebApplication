793e73c061b2879dfc48a955740e698694b243ad
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2022-02-28 12:27:52 +0100

    Index page development started
    Styling and HTML for index page.
    Finished grid, header, title

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <link href="./style/style.css" type="text/css" rel="stylesheet">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Project Battle Bots</title>
+        <?php
+            echo "
+            <style>
+                .header-style .nav .menu .home{
+                color: #83c0ff;
+                }
+                .header-style .nav .menu .home:hover{
+                color: #0386FF;
+                text-decoration: none;
+                }
+            </style>";
+        ?>
+    </head>
+    <body>
+        <!--Header of the page with code from a sepearate page-->
+        <?php include_once "header.html"; ?>
+
+        <!--Container for the whole page-->
+        <div id="indexContainer">
+
+            <!--Center gallery with bot picture and title-->
+            <div id="indexCenterGallery">
+                <div id="indexCenterTitle">
+                    <h1>Project <br> Battle Bots</h1>
+                </div>
+
+            </div>
+
+        </div>
+    </body>
+</html>
