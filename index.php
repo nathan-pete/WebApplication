b5441729b0e1b2b2e6741a3f8f4286af226745f5
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2022-03-02 12:47:39 +0100

    Added Titile, Images
    Scroll group still requires work
    Footer, Learn more buttons are missing

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Project Battle Bots</title>
+        <!--Highlight the page the user has open. Home in this case-->
         <?php
             echo "
             <style>
@@ -30,8 +31,33 @@
             <div id="indexCenterGallery">
                 <div id="indexCenterTitle">
                     <h1>Project <br> Battle Bots</h1>
+                    <p id="indexCenterSubtitle">By NHL Stenden</p>
                 </div>
 
+                <div id="indexCenterImage">
+                    <img src="assets/pasted-image.png" alt="Battle bot">
+                </div>
+            </div>
+
+            <!--Scroll group title-->
+            <div id="indexScrollTitle">
+                <h3>Welcome to Project Battle Bots!</h3>
+            </div>
+
+            <!--Scroll group-->
+            <div id="indexScrollGroup">
+                <!--PHP is used to allow easier connection to DB, which stores pictures os robots-->
+                <?php
+                    for ($a=0; $a<6; $a++) {
+                        echo '
+                        <div class="indexScrollItem">
+                            <img src="/assets/pasted-image.png" alt="Battle bot small">
+                        </div>
+    
+                        <!--An empty div to space items apart-->
+                        <div class="indexScrollSpacer"></div>';
+                    }
+                ?>
             </div>
 
         </div>
