bd3bec21aa689399145e4cf85a8126970e658c9c
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-03-09 22:08:17 +0100

    profile.php

---
+++ profile.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./style/style.css">
+  <link rel="icon" href="./assets/PBBwhite.png">
+  <title>Battle Bot Events</title>
+  <?php
+    echo "
+      <style>
+        .header-style .nav .menu .events{
+          color: #83c0ff;
+        }
+        .header-style .nav .menu .events:hover{
+          color: #0386FF;
+          text-decoration: none;
+        }
+      </style>";
+  ?>
+</head>
+<body>
+<div class="body">
+  <?php
+    include_once "header.html";
+  ?>
+  <div id="profileCenterTitle">
+        <h1>Robot <br> Profile</h1>
+    </div>
+    <pre>
+  <div class = "squarecolor">
+        <br>
+        <div class = "overall">
+            <div class = "squarecolorsmall">
+                </div>
+        <br>
+            <div class = "squarecolorsmall">
+                </div>
+        <br>
+            <div class = "squarecolorsmall">
+                </div>
+            </div>     
+        <div class = "overall2"> 
+            <div class = "squarecolorbig">
+                </div>
+            </div>
+    </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+<?php
+  include "footer.html";
+?>
+</body>
+</html>
