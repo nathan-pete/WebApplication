0d6c7875bd6afabe93c4f2eaa5e1f98cf811fc1b
Kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-03-09 16:35:07 +0100

    pasted-image.png not working this is why I used another image

--- index.php
+++ index.php
@@ -30,12 +30,12 @@
             <!--Center gallery with bot picture and title-->
             <div id="indexCenterGallery">
                 <div id="indexCenterTitle">
-                    <h1>Project <br> Battle Bots</h1>
+                    <h1 class="indexyellow">Project <br> Battle Bots</h1>
                     <p id="indexCenterSubtitle">By NHL Stenden</p>
                 </div>
 
                 <div id="indexCenterImage">
-                    <img src="./assets/pasted-image.png" alt="Battle bot">
+                    <img src="./assets/PBBblack.png" alt="Battle bot">
                 </div>
             </div>
 
@@ -47,26 +47,30 @@
             <!--Scroll group-->
             <div id="indexScrollGroup">
                 <!--PHP is used to allow easier connection to DB, which stores pictures os robots-->
-                
+                <?php
+                    for ($a=0; $a<6; $a++) {
+                        echo '
                         <div class="indexScrollItem">
-                            <img src="/assets/pasted-image.png" alt="Battle bot small">
+                            <img src="./assets/PBBblack.png" alt="Battle bot small">  
                         </div>
     
                         <!--An empty div to space items apart-->
                         <div class="indexScrollSpacer"></div>';
-                    
-                
+                    }
+                ?>
             </div>
-
-            <div id="indexMoreButton" onClick="location.href=''">
-                <p><b>More!</b></p>
+            
+        </div>
+        <div id="indexMoreButton">
+                <p><a class="indexa" href="#"><b>More!</b></a></p>
             </div>
-
+            
+        
             <div id="indexFooter">
                 <!--Footer of the page with code from a sepearate page-->
                 <?php include_once "footer.html"; ?>
             </div>
 
-        </div>
+        
     </body>
 </html>
