3b19e3be221ccbcd6b1d68db0185602e6f9df8d6
KrystianWiazowskiV2 <89790188+bouncychair@users.noreply.github.com>
2022-03-10 16:01:28 +0100

    Revert "Delete votegame.php"
    This reverts commit 6ecb5ba975e80031f4b3bac9fcba8f21af1b993f.

---
+++ votegame.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./style/style.css">
+  <link rel="icon" href="./assets/PBBwhite.png">
+  <title>Battle Bot Events</title>
+  <?php
+    echo "
+      <style>
+        .header-style .nav .menu .events{
+          color: #83c0ff;
+        }
+        .header-style .nav .menu .events:hover{
+          color: #0386FF;
+          text-decoration: none;
+        }
+      </style>";
+  ?>
+</head>
+<body>
+<div class="body">
+  <?php
+    include_once "header.html";
+  ?>
+  <div class="events-title">
+    <h1 style="font-weight: 700; pointer-events: none;">Vote for a game</h1>
+  </div>
+  <div class="events-content">
+
+    <div class="event-items">
+      <div class="event-img">
+        <img src="./assets/sumo.png" class="tweak" alt="Battle Bot - Sumo" height="110" width="135">
+        <div class="event-text">
+          <h3 class="event-h" style="padding-bottom:0.5%;">Sumo</h3>
+          <p class=" event-p"></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="event-items">
+      <div class="event-img">
+        <img src="./assets/line.png" class="tweak" alt="Battle Bot - Line Tracking" height="110" width="109">
+        <div class="event-text">
+          <h3 class="event-h" style="padding-bottom:0.5%;">Line Tracking</h3>
+          <p class=" event-p"></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="event-items">
+      <div class="event-img">
+        <img src="./assets/pen.png" class="tweak" alt="Battle Bot - Figure Drawing" height="110" width="135">
+        <div class="event-text">
+          <h3 class="event-h" style="padding-bottom:0.5%;">Figure Drawing</h3>
+          <p class=" event-p"></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="event-items">
+      <div class="event-img">
+        <img src="./assets/maze.png" class="tweak" alt="Battle Bot - Maze Race" height="110" width="135">
+        <div class="event-text">
+          <h3 class="event-h" style="padding-bottom:0.5%;">Maze Race</h3>
+          <p class=" event-p"></p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<br>
+<?php
+  include_once "footer.html";
+?>
+</body>
+</html>
