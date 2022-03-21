37fca0f0fde895064f081c18548f790123e62ba0
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-03-21 17:55:48 +0100

    profile

--- profiledraft.php
+++ profiledraft.php
@@ -1,74 +1,4 @@
-<?php 
-  include "connect.php";
-?>
-<?php 
-     if($_SERVER['REQUEST_METHOD'] === 'GET') {
-        if(isset($_GET['name'])) {
-          $name = $_GET['name'];
-
-          $info  = $conn -> prepare("SELECT * FROM robots WHERE robotName = ?");
-
-          if(!$info) {
-            die('Prepare failed' . mysqli_error($conn));
-          }
-
-          $info -> bind_param("s", $name);
-
-          if(!$info) {
-            die('Binding failed' . mysqli_error($conn));
-          }
-
-          $info -> execute();
-
-          if(!$info) {
-            die('Execute failed' . mysqli_error($conn));
-          }
-
-          $result = $info->get_result();
-
-          $infoR = $result -> fetch_all(MYSQLI_ASSOC);
-
-          $info -> close();
-          $conn -> close();
-        }
-     }
-?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="./style/style.css">
-  <link rel="icon" href="./assets/PBBwhite.png">
-  <title>Battle Bot Events</title>
-  <?php
-    echo "
-      <style>
-        .header-style .nav .menu .events{
-          color: #83c0ff;
-        }
-        .header-style .nav .menu .events:hover{
-          color: #0386FF;
-          text-decoration: none;
-        }
-      </style>";
-  ?>
-</head>
-<body>
-<div class="body">
-  <?php
-    include_once "header.php";
-  ?>
-  <div id="profileCenterTitle">
-    <h1>Robot <br> Profile</h1>
-  </div>
-  <div class="space">
-  </div>
-  <div class="squarecolor">
-    <br>
-    <div class="overall">
+<div class="overall">
       <div class="squarecolorsmall">
         <?php
           $conn = mysqli_connect("localhost", "root", "", "webapp");
@@ -185,9 +115,3 @@
         </div>
     </div>
 <?php
-  include_once "footer.html";
-?>
-    </div>
-  </div>
-</body>
-</html>
