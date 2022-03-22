0c50fb526ec1f6d3153fde65ad86138edbb469b8
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-03-22 23:40:40 +0100

    Update addEvent.php

--- addEvent.php
+++ addEvent.php
@@ -44,49 +44,6 @@
                             $acceptedFileTypes = ["image/png", "image/jpeg", "image/jpg"];
                             $uploadedFileType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES['image']['tmp_name']);
                             if(!empty($_POST['name']) && !empty($_POST['desc']) && !empty($_FILES['image'])) {
-                                if ($_FILES['image']['size']<2500000) {
-                                    if (strlen($_FILES['image']['name']) <= 35) {
-                                        if (in_array($uploadedFileType, $acceptedFileTypes)) {
-                                            $fileinfo = getimagesize($_FILES["image"]["tmp_name"]);
-                                            $width = $fileinfo[0];
-                                            $height = $fileinfo[1];
-                                            if ($width < 500 && $height < 500) {
-                                                if (!file_exists("./uploads/games/" . $_FILES['image']['name'])) {
-                                                    if (move_uploaded_file($_FILES['image']['tmp_name'], "./uploads/games/" . $_FILES['image']['name'])) {
-                                                        $name = $_POST['name'];
-                                                        $desc = $_POST['desc'];
-                                                        $img = $_FILES['image']['name'];
-                                                        $sql = "INSERT INTO games (name, Descrption, picture) VALUES (?,?,?)";
-                                                        if ($stmt = mysqli_prepare($conn, $sql)) {
-                                                            mysqli_stmt_bind_param($stmt, "sss", $name, $desc, $img);
-                                                            if (!mysqli_stmt_execute($stmt)) {
-                                                                echo "Error executing query" . mysqli_error($conn);
-                                                                die(); //die if we cant execute statement
-                                                            }else {
-                                                                echo "Event created successfully";
-                                                            }
-                                                        }else {
-                                                            echo "Error executing query" . mysqli_error($conn);
-                                                            die(); //die if we cant execute statement
-                                                        }
-                                                    }else {
-                                                        echo "Something went wrong, please try again";
-                                                    }
-                                                }else {
-                                                    echo "<br><p>" . $_FILES['image']['name'] . " already exists.</p>";
-                                                }
-                                            }else {
-                                                echo "Image dimensions can't exceed 500x500!";
-                                            }
-                                        }else {
-                                            echo "Invalid image type! Only png, jpeg and jpg are permitted.";
-                                        }
-                                    }else {
-                                        echo "File name must not exceed 35 Characters.";
-                                    }
-                                }else {
-                                    echo "Image should not exceed 3MB.";
-                                }
                             }else {
                                 echo "Please fill in all fields!";
                             }
