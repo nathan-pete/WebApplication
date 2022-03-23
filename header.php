28948616b457a4b7be744cd9b12869abe4170467
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-03-23 06:18:06 +0100

    Added header age check for the bets page v1

--- header.php
+++ header.php
@@ -9,15 +9,20 @@
       <li class="line"> &VerticalLine;</li>
       <li><a href="leaderboard.php" class="lead">Leaderboard</a></li>
       <li class="line"> &VerticalLine;</li>
-      <li><a href="#" class="bets">Bets</a></li>
-      <li class="line"> &VerticalLine;</li>
       <?php
+        if (isset($_SESSION['isLogged'])) {
+            include "connect.php";
+            $sql = "SELECT DoB FROM users WHERE userID = ?";
+        } else {
+            echo ' <li><a href="login.php" class="bets">Bets</a></li>';
+        }
+        echo "<li class='line'> &VerticalLine;</li>";
         if (isset($_SESSION['isLogged'])) {
           echo '<li><a href="usrpnl.php" class="login">' . $_SESSION['isLogged'] . '</a></li>';
         } else {
           echo '<li><a href="login.php" class="login">Login</a></li>';
         }
-      ?>
+     ?>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     </div>
     <div class="h-sign">
