8db3dcee1baa6292145e35cb484d0e04c5d07fa9
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-03-23 11:34:26 +0100

    again test version

--- image.php
+++ image.php
@@ -6,13 +6,11 @@ include "connect.php";
   $id = (int)$_GET['id']; 
   if ( $id > 0 ) { 
     $query = "SELECT `content` FROM `images` WHERE `id`=".$id; 
-    // Выполняем запрос и получаем файл 
+    
     $res = mysql_query($query); 
     if ( mysql_num_rows( $res ) == 1 ) { 
       $image = mysql_fetch_array($res); 
-      // Отсылаем браузеру заголовок, сообщающий о том, что сейчас будет передаваться файл изображения
-      header("Content-type: image/jpeg"); 
-      // И  передаем сам файл 
+      header("Content-type: image/jpeg");
       echo $image['content']; 
     } 
   } 
