670cf1874ff354607450e4be1165a2bfe4a65a19
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-03-27 00:08:45 +0100

    bug display v2

--- betting18p.php
+++ betting18p.php
@@ -47,10 +47,10 @@
                           mysqli_stmt_bind_param($stmt, "s", $robotName);
                           if (mysqli_stmt_execute($stmt)) {
                           $result =  mysqli_stmt_get_result($stmt);
-                            while ($row = mysqli_fetch_assoc($result)) {
-                              if (mysqli_num_rows($result) == 0) {
-                                echo "<div class='space-event'>Sorry, this robot has no available pictures.</div>";
-                              } else {
+                            if (mysqli_num_rows($result) == 0) {
+                              echo "<div class='space-event'>Sorry, this robot has no available pictures.</div>";
+                            } else {
+                              while ($row = mysqli_fetch_assoc($result)) {
                                 echo "<p><img src='./uploads/robots/" . $row['robotPicture'] ."' alt='Picture of the robot'></p>";
                               }
                             }
