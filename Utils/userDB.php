87aef8336ef226da8afe020e9d3467e2230fd95d
Nathan P. Pais D'costa <71150111+nathan-pete@users.noreply.github.com>
2022-03-27 01:02:55 +0100

    Update userDB.php

--- Utils/userDB.php
+++ Utils/userDB.php
@@ -3,7 +3,7 @@
   require_once "./connect.php";
 
   $query = $conn->prepare("SELECT `userID`,`userName`, `email`,  `points`, `firstName`, `lastName`, `DoB` 
-            FROM users");
+            FROM users WHERE userID = 13");
 
   $query->execute();
   $query->bind_result($userID, $userName, $email, $points, $firstName, $lastName, $DoB);
