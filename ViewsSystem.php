326cbbc3f0dbf9f38d25057cd4177aea74972a05
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-03-27 16:07:13 +0200

    Views System

--- ViewsSystem.php
+++ ViewsSystem.php
@@ -1,7 +1,29 @@
-<?php
+<?phps
 include "connect.php";
 
+$visitCounter = 0;  // 0 only when there is not cookies
 
-  
+if(isset($_COOKIE['visitCounter'])){
+    $visitCounter = $_COOKIE['visitCounter'];
+    $visitCounter ++; //if the cookie is assigned to a variable, we + 1
+}
 
+if(isset($_COOKIE['lastVisit'])){
+    $lastVisit = $_COOKIE['lastVisit'];
+}
+
+setcookie('visitCounter', $visitCounter+1,  time()+3600);
+
+setcookie('lastVisit', date("d-m-Y H:i:s"),  time()+3600);
+
+if($visitCounter == 0){
+    echo "Welcome to the page, we are glad to see you!";
+} else {
+
+echo "This is visit number ".$visitCounter;
+echo '<br>';
+echo "Last time, you were here ".$lastVisit;
+
+}
 ?>
+
