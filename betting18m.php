62dfae4aa15b6584426a65da16d18b7de3ae37b3
radoychev <62029581+radoychev@users.noreply.github.com>
2022-03-28 23:41:25 +0200

    Added stuff

--- betting18m.php
+++ betting18m.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="body">
+        <?php
+            require "header.php";
+        ?>
+        <div class="main_box_betting">
+            <div class="small_betting_box">
+                
+            </div>
+        </div>
+
+    </div>
     
 </body>
 </html>
