ec70a5b75749855e73b311dc8babf86584548d65
Geniools <30665942+Geniools@users.noreply.github.com>
2022-03-28 11:28:19 +0200

    Just intendations...

--- leaderboard.php
+++ leaderboard.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="./style/style.css">
-  <link rel="icon" href="./assets/PBBwhite.png">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <title>Battle Bot Events</title>
-  <?php
-    echo "
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./style/style.css">
+	<link rel="icon" href="./assets/PBBwhite.png">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<title>Battle Bot Events</title>
+	<?php
+		echo "
       <style>
         .header-style .nav .menu .lead{
           color: #83c0ff;
@@ -19,35 +19,35 @@
           text-decoration: none;
         }
       </style>";
-  ?>
+	?>
 </head>
 <body>
 <div class="body">
-  <?php
-    include_once "header.php";
-  ?>
-  <div class="events-title">
-    <h1 style="font-weight: 700; pointer-events: none;">Leaderboard</h1>
-    <div class="space-event"></div>
-    <p class="leadnote">**Note: Leaderboard Refresh every Two seconds**</p>
-  </div>
-  <div class="space-event"></div>
-  <div class="center">
-    <table class="leaderboard">
-    </table>
-  </div>
+	<?php
+		include_once "header.php";
+	?>
+	<div class="events-title">
+		<h1 style="font-weight: 700; pointer-events: none;">Leaderboard</h1>
+		<div class="space-event"></div>
+		<p class="leadnote">**Note: Leaderboard Refresh every Two seconds**</p>
+	</div>
+	<div class="space-event"></div>
+	<div class="center">
+		<table class="leaderboard">
+		</table>
+	</div>
 </div>
 <script>
-    setInterval(function () {
-        $('.leaderboard').load('./Utils/lbdData.php');
-    }, 2000)
+	setInterval(function () {
+		$('.leaderboard').load('./Utils/lbdData.php');
+	}, 2000)
 </script>
 <div class="space-pass"></div>
 
 
 <br>
 <?php
-  include_once "footer.html";
+	include_once "footer.html";
 ?>
 </body>
 </html>
--- votegame.php
+++ votegame.php
@@ -1,50 +1,50 @@
 <?php
-  include "connect.php";
+	include "connect.php";
 ?>
 
 <?php
-  if (isset($_GET['name'])) {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-      $name = $_GET['name'];
-
-      $query = $conn->prepare("SELECT `votes` FROM games WHERE name = ?");
-      mysqli_stmt_bind_param($query, 's', $name);
-      $query->execute();
-
-      $query->bind_result($amountOfVotes);
-      $query->fetch();
-
-      $calc_vote = $amountOfVotes + 1;
-      mysqli_stmt_close($query);
-
-      $update_query = mysqli_prepare($conn, "UPDATE games SET votes = ? WHERE `name`=?");
-      mysqli_stmt_bind_param($update_query, 'is', $calc_vote, $name);
-
-      mysqli_stmt_execute($update_query);
-      $update_query->execute();
-    }
-  }
-  /*try{
-   //do something
-  }catch{
-   echo mysqli_error($conn);
-  }*/
+	if (isset($_GET['name'])) {
+		if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+			$name = $_GET['name'];
+			
+			$query = $conn->prepare("SELECT `votes` FROM games WHERE name = ?");
+			mysqli_stmt_bind_param($query, 's', $name);
+			$query->execute();
+			
+			$query->bind_result($amountOfVotes);
+			$query->fetch();
+			
+			$calc_vote = $amountOfVotes + 1;
+			mysqli_stmt_close($query);
+			
+			$update_query = mysqli_prepare($conn, "UPDATE games SET votes = ? WHERE `name`=?");
+			mysqli_stmt_bind_param($update_query, 'is', $calc_vote, $name);
+			
+			mysqli_stmt_execute($update_query);
+			$update_query->execute();
+		}
+	}
+	/*try{
+	 //do something
+	}catch{
+	 echo mysqli_error($conn);
+	}*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="./style/style.css">
-  <link rel="icon" href="./assets/PBBwhite.png">
-
-  <link href="http://designify.me/code-snippets/animated-buttons/css/btns.css" rel="stylesheet">
-  <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-  <title>Battle Bot Events</title>
-  <?php
-    echo "
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./style/style.css">
+	<link rel="icon" href="./assets/PBBwhite.png">
+	
+	<link href="http://designify.me/code-snippets/animated-buttons/css/btns.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<title>Battle Bot Events</title>
+	<?php
+		echo "
       <style>
         .header-style .nav .menu .events{
           color: #83c0ff;
@@ -54,32 +54,32 @@
           text-decoration: none;
         }
       </style>";
-  ?>
+	?>
 </head>
 <body>
 <div class="body">
-  <?php 
-    include_once "header.php";
-  ?>
-  <div id="profileCenterTitle">
-    <h1>Vote the game</h1>
-    <div class = "statistics">
-      <form method="LINK" action="ViewsSystem.php">
-    <button class="custom-btn btn-6"><span>Statistics</span></button>
-</form>
-      </div>
-  </div>
-  <?php
-
-    $query = $conn->prepare("SELECT `name`, `picture`, `votes` FROM games");
-
-    $query->execute();
-    $query->bind_result($name, $picture, $votes);
-    while ($query->fetch()) {
-
-      echo '<div class="event-items">
+	<?php
+		include_once "header.php";
+	?>
+	<div id="profileCenterTitle">
+		<h1>Vote the game</h1>
+		<div class="statistics">
+			<form method="LINK" action="ViewsSystem.php">
+				<button class="custom-btn btn-6"><span>Statistics</span></button>
+			</form>
+		</div>
+	</div>
+	<?php
+		
+		$query = $conn->prepare("SELECT `name`, `picture`, `votes` FROM games");
+		
+		$query->execute();
+		$query->bind_result($name, $picture, $votes);
+		while ($query->fetch()) {
+			
+			echo '<div class="event-items">
       <div class="event-img">
-        <img class="voteimg" src=assets/' . $picture. '>
+        <img class="voteimg" src=assets/' . $picture . '>
         <div class="event-text">
           <h3 class="event-h" style="padding-bottom:0.5%;">' . $name . '</h3>
           <p class=" event-p"></p>
@@ -92,24 +92,24 @@
       <h3>Votes: ' . $votes . '</h3> 
         </div>
     </div>';
-    }
-    
-
-    //if (mysqli_stmt_num_rows() > 0) {
-    //while(mysqli_stmt_fetch()){
-
-    mysqli_stmt_close($query);
-?>
-  <!-- /container -->
-  <!-- Bootstrap core JavaScript
-      ================================================== -->
-  <!-- Placed at the end of the document so the pages load faster -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  <br>
+		}
+		
+		
+		//if (mysqli_stmt_num_rows() > 0) {
+		//while(mysqli_stmt_fetch()){
+		
+		mysqli_stmt_close($query);
+	?>
+	<!-- /container -->
+	<!-- Bootstrap core JavaScript
+		================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<br>
 </div>
 <?php
-    include_once "footer.html";
-  ?>
+	include_once "footer.html";
+?>
 </body>
 </html>
