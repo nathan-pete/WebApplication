90b11125b5349b9ccdef8082c58f415426b14fb8
TerryIoannou <91316685+TerryIoannou@users.noreply.github.com>
2022-03-30 11:24:01 +0200

    updated sidebar for admin

--- SideBar.php
+++ SideBar.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="styling.css">
-	<title>Admin Panel</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styling.css">
+    <title>Admin Panel</title>
 </head>
 <body class="backgroundsidebar">
-<div class="Sidebar">
-	<a href="Admin.php" clas>Admin Home</a>
-	<br>
-	<a href="Userpanel.php">User Panel</a>
-	<br>
-	<a href="Index.php">Live stream</a>
-	<br>
-	<a href="Robotpanel.php">Robot Panel</a>
-	<br>
-	<a href="commentpanel.php">Comments</a>
-</div>
+    <div class="Sidebar">
+    <div>
+    <img src="Jarvis.gif" class="jarvis">
+    </div>
+    <a href="Admin.php" clas>Admin Home</a>
+    <br>
+    <a href="Userpaneladmin.php">User Panel</a>
+    <br>
+    <a href="Index.php">Live stream</a>
+    <br>
+    <a href="Robotpanel.php">Robot Panel</a>
+    <br>
+    <a href="commentpanel.php">Comments</a>
+    <br>
+    <a href="feedback.php">FeedBack</a>
+    <br>
+    <a href="contact_admin_overview.php">Contact Panel</a>
+    </div>
+    
 </body>
 </html>
