844f9997fd93df9da09bfea68ad05f5c83b50cea
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-03-30 14:25:42 +0200

    spent point sysytem

--- spentPoints.php
+++ spentPoints.php
@@ -1,4 +1,9 @@
 <?php
+
+  /*
+    Send a GET request via form 
+  */
+
     session_start();
 
     include 'connect.php';
@@ -30,11 +35,15 @@
       </style>";
   ?>
 </head>
-<body>
+<body>      
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
             <input type="submit" name="smile" value = "smile" />
             <input type="submit" name="laugh" value = "laugh"/>
             <input type="submit" name="angry" value = "angry" />
+            <input type="submit" name="love" value = "love" />
+            <input type="submit" name="sad" value = "sad" />
+            <input type="submit" name="wink" value = "wink" />
+            <input type="submit" name="strong" value = "strong" />
             </form>
 
             <?php
@@ -54,12 +63,14 @@
                                   if ($sql = mysqli_prepare($conn, "UPDATE users SET points = ? WHERE userID = ?"))
                                   {
                                     $newPoints = $row['points'] - $action;
-                                    $message = "smile :)";
+                                    $message = "(❛ ͜ʖ ͡❛)";
+                                    
                                     mysqli_stmt_bind_param($sql, 'ii', $newPoints, $userID);
                                     //echo $userID;
                                     //echo $newPoints;
-                                    echo "<form>
-                                    <textarea>$message</textarea>
+                                    echo "<form action='spentPoints.php' method='POST'>
+                                        <textarea id='test' name = 'comments' rows='10' cols='50';>$message</textarea>
+                                        <input type='submit' name='submit'>
                                     </form>";
                                     if (mysqli_stmt_execute($sql))
                                     {
@@ -70,19 +81,27 @@
                                   } else {
                                     echo "error" . mysqli_error($conn);
                                   }
-                                } else {echo "you don't have enough points";}
+                                } else {echo "you don't have enough points";
+                                }
                               }
-                            }
-                          }
-                        } 
 
-                              
-                              
-                              if (isset($_POST["submit"])) {
-                                $comment = $_POST["comments"];
-                                if (!empty($comment)) {
-                                      $sql = mysqli_prepare($conn, "INSERT INTO `Comments` (userID, `message`) VALUES (?, ?)");
-                                      mysqli_stmt_bind_param($sql, 'is', $userID, $message);
+                              if (isset($_POST['laugh']))
+                              {
+                                $action = 10;
+                                if ($row['points'] > $action)
+                                {
+                                  if ($sql = mysqli_prepare($conn, "UPDATE users SET points = ? WHERE userID = ?"))
+                                  {
+                                    $newPoints = $row['points'] - $action;
+                                    $message = "( ͡❛ ⏥ ͡❛)";
+                                    
+                                    mysqli_stmt_bind_param($sql, 'ii', $newPoints, $userID);
+                                    //echo $userID;
+                                    //echo $newPoints;
+                                    echo "<form action='spentPoints.php' method='POST'>
+                                        <textarea id='test' name = 'comments' rows='10' cols='50';>$message</textarea>
+                                        <input type='submit' name='submit'>
+                                    </form>";
                                     if (mysqli_stmt_execute($sql))
                                     {
                                       mysqli_stmt_close($sql);
@@ -92,10 +111,181 @@
                                   } else {
                                     echo "error" . mysqli_error($conn);
                                   }
+                                } else {echo "you don't have enough points";
+                                }
+                              }
+                              
+                              if (isset($_POST['angry']))
+                              {
+                                $action = 10;
+                                if ($row['points'] > $action)
+                                {
+                                  if ($sql = mysqli_prepare($conn, "UPDATE users SET points = ? WHERE userID = ?"))
+                                  {
+                                    $newPoints = $row['points'] - $action;
+                                    $message = "(︡* ⏥*︠)";
+                                    
+                                    mysqli_stmt_bind_param($sql, 'ii', $newPoints, $userID);
+                                    //echo $userID;
+                                    //echo $newPoints;
+                                    echo "<form action='spentPoints.php' method='POST'>
+                                        <textarea id='test' name = 'comments' rows='10' cols='50';>$message</textarea>
+                                        <input type='submit' name='submit'>
+                                    </form>";
+                                    if (mysqli_stmt_execute($sql))
+                                    {
+                                      mysqli_stmt_close($sql);
+                                    } else {
+                                      echo "error" . mysqli_error($conn);
+                                    }
+                                  } else {
+                                    echo "error" . mysqli_error($conn);
+                                  }
+                                } else {echo "you don't have enough points";
+                                }
+                              }
+
+                              if (isset($_POST['love']))
+                              {
+                                $action = 10;
+                                if ($row['points'] > $action)
+                                {
+                                  if ($sql = mysqli_prepare($conn, "UPDATE users SET points = ? WHERE userID = ?"))
+                                  {
+                                    $newPoints = $row['points'] - $action;
+                                    $message = "(ɔ◔‿◔)ɔ ♥";
+                                    
+                                    mysqli_stmt_bind_param($sql, 'ii', $newPoints, $userID);
+                                    //echo $userID;
+                                    //echo $newPoints;
+                                    echo "<form action='spentPoints.php' method='POST'>
+                                        <textarea id='test' name = 'comments' rows='10' cols='50';>$message</textarea>
+                                        <input type='submit' name='submit'>
+                                    </form>";
+                                    if (mysqli_stmt_execute($sql))
+                                    {
+                                      mysqli_stmt_close($sql);
+                                    } else {
+                                      echo "error" . mysqli_error($conn);
+                                    }
+                                  } else {
+                                    echo "error" . mysqli_error($conn);
+                                  }
+                                } else {echo "you don't have enough points";
+                                }
+                              }
+
+                              if (isset($_POST['sad']))
+                              {
+                                $action = 10;
+                                if ($row['points'] > $action)
+                                {
+                                  if ($sql = mysqli_prepare($conn, "UPDATE users SET points = ? WHERE userID = ?"))
+                                  {
+                                    $newPoints = $row['points'] - $action;
+                                    $message = "( ˘︹˘ )";
+                                    
+                                    mysqli_stmt_bind_param($sql, 'ii', $newPoints, $userID);
+                                    //echo $userID;
+                                    //echo $newPoints;
+                                    echo "<form action='spentPoints.php' method='POST'>
+                                        <textarea id='test' name = 'comments' rows='10' cols='50';>$message</textarea>
+                                        <input type='submit' name='submit'>
+                                    </form>";
+                                    if (mysqli_stmt_execute($sql))
+                                    {
+                                      mysqli_stmt_close($sql);
+                                    } else {
+                                      echo "error" . mysqli_error($conn);
+                                    }
+                                  } else {
+                                    echo "error" . mysqli_error($conn);
+                                  }
+                                } else {echo "you don't have enough points";
+                                }
+                              }
+
+                              if (isset($_POST['wink']))
+                              {
+                                $action = 10;
+                                if ($row['points'] > $action)
+                                {
+                                  if ($sql = mysqli_prepare($conn, "UPDATE users SET points = ? WHERE userID = ?"))
+                                  {
+                                    $newPoints = $row['points'] - $action;
+                                    $message = "(>‿◠)✌";
+                                    
+                                    mysqli_stmt_bind_param($sql, 'ii', $newPoints, $userID);
+                                    //echo $userID;
+                                    //echo $newPoints;
+                                    echo "<form action='spentPoints.php' method='POST'>
+                                        <textarea id='test' name = 'comments' rows='10' cols='50';>$message</textarea>
+                                        <input type='submit' name='submit'>
+                                    </form>";
+                                    if (mysqli_stmt_execute($sql))
+                                    {
+                                      mysqli_stmt_close($sql);
+                                    } else {
+                                      echo "error" . mysqli_error($conn);
+                                    }
+                                  } else {
+                                    echo "error" . mysqli_error($conn);
+                                  }
+                                } else {echo "you don't have enough points";
+                                }
+                              }
+
+                              if (isset($_POST['strong']))
+                              {
+                                $action = 10;
+                                if ($row['points'] > $action)
+                                {
+                                  if ($sql = mysqli_prepare($conn, "UPDATE users SET points = ? WHERE userID = ?"))
+                                  {
+                                    $newPoints = $row['points'] - $action;
+                                    $message = "(ง︡'-'︠)ง";
+                                    
+                                    mysqli_stmt_bind_param($sql, 'ii', $newPoints, $userID);
+                                    //echo $userID;
+                                    //echo $newPoints;
+                                    echo "<form action='spentPoints.php' method='POST'>
+                                        <textarea id='test' name = 'comments' rows='10' cols='50';>$message</textarea>
+                                        <input type='submit' name='submit'>
+                                    </form>";
+                                    if (mysqli_stmt_execute($sql))
+                                    {
+                                      mysqli_stmt_close($sql);
+                                    } else {
+                                      echo "error" . mysqli_error($conn);
+                                    }
+                                  } else {
+                                    echo "error" . mysqli_error($conn);
+                                  }
+                                } else {echo "you don't have enough points";
+                                }
+                              }
                             }
+                          }
+                        }
+                              if (isset($_POST["submit"])) {
+                                //$smile = $_GET['smile'];
+                                //var_dump($_POST);
+                                $comment = $_POST["comments"];
+                                if (!empty($comment)) {
+                                      $sql = mysqli_prepare($conn, "INSERT INTO `Comments` (userID, `message`) VALUES (?, ?)");
+                                      mysqli_stmt_bind_param($sql, 'is', $userID, $comment);
+                                    if (mysqli_stmt_execute($sql))
+                                    {
+                                      mysqli_stmt_close($sql);
+                                    } else {
+                                      echo "error" . mysqli_error($conn);
+                                    }
+                                  } else {
+                                    echo "error" . mysqli_error($conn);
+                                  }
+                            }                        
 ?>
-<form action="#"class="comments_form" method="POST">
-        <button type="submit" name="submit" class="comments_button">Add message</button>   
+<form action="<?php $_SERVER['PHP_SELF']; ?>" class="comments_form" method="POST">
     </form>
     </body>
 </html>
