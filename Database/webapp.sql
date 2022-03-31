f7afeb5e8671702164bf01daa4bf42e69378fb21
Geniools <30665942+Geniools@users.noreply.github.com>
2022-03-31 15:29:52 +0200

    Updated database

--- Database/webapp.sql
+++ Database/webapp.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2022 at 04:14 PM
+-- Generation Time: Mar 31, 2022 at 03:28 PM
 -- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.0
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `webapp`
 --
-CREATE DATABASE IF NOT EXISTS `webapp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `webapp`;
 
 -- --------------------------------------------------------
 
@@ -48,6 +46,18 @@ CREATE TABLE `comments` (
   `userID` int(11) NOT NULL,
   `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `userID`, `message`) VALUES
+(80, 16, 'bro'),
+(81, 16, 'hello'),
+(82, 14, 'bro'),
+(83, 14, 'hoooo'),
+(84, 14, 'peee'),
+(85, 14, 'Terry');
 
 -- --------------------------------------------------------
 
@@ -111,9 +121,23 @@ CREATE TABLE `robots` (
   `sound` blob NOT NULL,
   `robotPicture` varchar(50) NOT NULL,
   `teamName` varchar(255) NOT NULL,
-  `points` int(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `points` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `mac` varchar(40) NOT NULL,
+  `ip` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `robots`
+--
+
+INSERT INTO `robots` (`robotName`, `serialNum`, `sound`, `robotPicture`, `teamName`, `points`, `password`, `mac`, `ip`) VALUES
+('Robot_F', '6', '', 'path', 'Robot_F', 100, '$2y$10$uvWMvrSv22MVOOPbIZm..uG8KCCu1lbpuReejxjfEo8Vqg1H4atbK', '10:52:1c:5d:49:d0', '192.168.10.110'),
+('Emmanuel', '5', '', 'path', 'Robot_E', 100, '$2y$10$EIffM/a47HOsmNsgZTKAmOEJ9CPFjr1XU6ZBRq.HYoMm2r/xD45S2', '10:52:1c:5e:0c:94', '192.168.10.109'),
+('Robot_A', '1', '', 'path', 'Robot_A', 100, '$2y$10$QS9j7VnYIFVKuITFzDp2YOVLWm2dBAZTlPlNEB/hS2rvOENUkeJYO', '24:0a:c4:61:b1:48', '192.168.10.105'),
+('Robot_C', '3', '', 'path', 'Robot_C', 100, '$2y$10$XmARxHqJJPXJmypF1LHV.u46cou61Qv/KpV5ZZm1ToBcjltr0mGwa', '7C:9E:BD:5F:7C:7C', '192.168.10.107'),
+('Robot_D', '4', '', 'path', 'Robot_D', 100, '$2y$10$.iukM0OPqo/D1JGiTR68seV3jkEMB39mPxltO.8kNrqSoYa1egJ8u', '84:CC:A8:7E:C7:FC', '192.168.10.108'),
+('Robot_B', '2', '', 'path', 'Robot_B', 100, '$2y$10$/eLCWTi77BxI6OywAzf3/e4H.Mi8FVorHEZQKw4FvGE70KXsqoIqi', 'ac:67:b2:36:29:7c', '192.168.10.106');
 
 -- --------------------------------------------------------
 
@@ -131,8 +155,18 @@ CREATE TABLE `users` (
   `DoB` tinyint(1) NOT NULL,
   `password` varchar(255) NOT NULL,
   `profilePic` varchar(30) NOT NULL DEFAULT 'defaultpfp.png',
+  `vote` int(30) NOT NULL DEFAULT 0,
   `status` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `userName`, `email`, `points`, `firstName`, `lastName`, `DoB`, `password`, `profilePic`, `vote`, `status`) VALUES
+(14, 'bro', 'bro@gmail.com', 500, 'bro', 'bro', 1, '$2y$10$1VO7CZvnQ4/PO.fMljhkYOWcfheRbbVEhxGQooNz1oBrvR2BQM9mK', 'defaultpfp.png', 0, 'user'),
+(15, 'bro', 'bro@bro.com', 500, 'rbo', 'rbo', 1, '$2y$10$k54n/UD6J.xBDInRgCUBv.VnZI9SnuvFhfymZBdKsTBqMwBI7MOzC', 'defaultpfp.png', 0, 'user'),
+(16, 'violetta', 'vio@vio.com', 500, 'violetta', 'violetta', 1, '$2y$10$jqaKJnA/K.J2zHmrLt73GusQ6aao4vIi/3MtQwc26qYE6nYi3bKw6', 'defaultpfp.png', 0, 'administrator');
 
 --
 -- Indexes for dumped tables
@@ -158,6 +192,12 @@ ALTER TABLE `feedback`
   ADD PRIMARY KEY (`IDFeedback`);
 
 --
+-- Indexes for table `robots`
+--
+ALTER TABLE `robots`
+  ADD PRIMARY KEY (`mac`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -171,7 +211,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- AUTO_INCREMENT for table `contact_admin`
@@ -189,7 +229,7 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `userID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
