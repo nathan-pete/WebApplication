b45e38c029c35431d985b27c152b823b3a0143a4
Geniools <30665942+Geniools@users.noreply.github.com>
2022-04-01 19:24:15 +0200

    Added invisible class for storing the ip address of the robot. Ajax can send the ip of the robot

--- js/robotControl.js
+++ js/robotControl.js
@@ -1,14 +1,15 @@
 $(function () {
-  $(".robotControl").on("click", function () {
-    let request = $(this).text();
-    console.log(request);
-    $.ajax({
-      type: "post",
-      url: "./robotControl.php",
-      data: {
-        "httpRequest": request,
-        "ipAddress": "<?= $_SESSION["robotIP"]; ?>"
-      },
-    });
-  })
+    $(".robotControl").on("click", function () {
+        let request = $(this).text();
+        let ipAddress = $("robotIPAddress").text();
+        console.log(request);
+        $.ajax({
+            type: "post",
+            url: "./robotControl.php",
+            data: {
+                "httpRequest": request,
+                "ipAddress": ipAddress
+            },
+        });
+    })
 })
