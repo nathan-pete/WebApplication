50f883920c688f2aa5d9427a61ec323cbfce142f
liafernando <ameli.masewge.fernando@student.nhlstenden.com>
2022-04-08 22:59:46 +0200

    Everything

---
+++ Lanuage translations/NL.php
@@ -0,0 +1,88 @@
+<?php
+    
+    //Home page translation//
+//header//
+    $lang = array (
+        "MENUBAR" => "MENUBALK",
+        "HOME" => "HOME",
+        "EVENTS" => "EVENEMENTEN",
+        "LEADERBOARD" => "SCOREBORD",
+        "PROJECT BATTLEBOTS" => "PROJECT GEVECHTSROBOTS",
+        "WELCOME TO PROJECT BATTLEBOTS!" =>  "WELKOM BIJ PROJECT GEVECHTSROBOTS!" 
+        "LOGIN" => "LOG IN",
+
+    );
+
+//Footer//
+
+    $lang = array (
+         "CONTACT US!" => "NEEM CONTACT OP!",
+
+     );   
+
+//Events page//
+        
+    $lang = array (
+         "There are no availave events" => "Er zijn geen beschikbare",
+
+     );   
+        
+//Leaderboard page//
+  
+    $lang = array (
+         "LEADERBOARD" => "SCOREBORD",
+         "POSITION" => "POSITIE",
+         "ROBOTNAME" => "ROBOTNAAM",
+         "POINTS" => "PUNTEN",
+
+     );   
+       
+//Mini games page//
+
+    $lang = array (
+         "SCORES" => "SCORES",
+         "RULES" => "REGLEMENT",
+
+     );  
+
+//bets and login page//
+ 
+    $lang = array (
+         "LOGIN" => "LOG IN",
+         "I HAVE A ROBOT" => "IK HEB EEN ROBOT",
+         "I AM A VIEWER" => "I BEN EEN KIJKER",
+         "Serial no" =>  "Serieel no",
+         "Enter Password" =>  "Voer wachtwoord in",
+         "Email" => "E-mail",
+         "FILL IN ALL THE FIELDS" => "VUL ALLE VELDEN IN",
+         "FORGOTTEN PASSWORD" => "VERGETEN WACHTWOORD",
+         "REGISTER A NEW ROBOT" => "REGISTREER EEN NIEUWE ROBOT",
+         "REGISTER A NEW USER" => "REGISTEER EEN NIEUWE GEBRUIKER",
+         "FILL OUT THIS FIELD" => "VUL DIT VELD IN",
+
+     );              
+//register a robot//
+
+     $lang = array (
+         "Register Robot" => "Registreer robot",
+         "Robot name" => "Robot naam" ,
+         "Serial Number" =>  "Serieel Nummer",
+         "Team name" => "Team naam",
+         "Password" => "Wachtwoord",
+         "Confirm pasword" =>  "Bevestigen Wachtwoord",
+         "Create account" => "Maak account",
+
+     );              
+ 
+    $lang = array (
+
+        "Register User" => "Gebruiker registreren",
+        "Username" => "Gebruikersnaam" ,
+        "First name" => "Voornaam",
+        "Last name" => "Achternaam" ,
+        "Date of Birth" => "Geboortedatum",
+        "Email" => "E-mail",
+        "Register" => "Registreer" ,
+
+    );
+?>    
