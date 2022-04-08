d355371ea68cfcfc0b8af6139865edefc3329768
liafernando <ameli.masewge.fernando@student.nhlstenden.com>
2022-04-08 19:10:55 +0200

    FinalLanuageTranslations

---
+++ Lanuage translations/FR.php
@@ -0,0 +1,88 @@
+<?php
+//Home page translation//
+//header//
+    $lang = array (
+        "MENUBAR" => "barre de menu",
+        "HOME" => "HOME",
+        "EVENTS" => "Événements",
+        "LEADERBOARD" => "classement",
+        "PROJECT BATTLEBOTS" => "robots de combat de projet",
+        "WELCOME TO PROJECT BATTLEBOTS!" =>  "bienvenue dans le projet battlebots" 
+        "LOGIN" => "entrer",
+
+    );
+
+//Footer//
+
+    $lang = array (
+         "CONTACT US!" => "свържете се с нас!",
+
+     );   
+
+//Events page//
+        
+    $lang = array (
+         "EVENTS" => "Nous contacter!",
+         "There are no availave events" => "Il n'y a pas d'événements disponibles",
+
+     );   
+        
+//Leaderboard page//
+  
+    $lang = array (
+         "LEADERBOARD" => "classement",
+         "POSITION" => "position",
+         "ROBOTNAME" => "nom du robot",
+         "POINTS" => "les points",
+
+     );   
+       
+//Mini games page//
+
+    $lang = array (
+         "SCORES" => "gagner",
+         "RULES" => "des règles",
+
+     );  
+
+//bets and login page//
+ 
+    $lang = array (
+         "LOGIN" => "entrer",
+         "I HAVE A ROBOT" => "j'ai un robot",
+         "I AM A VIEWER" => "je suis spectateur",
+         "Serial no" =>  "numéro de série",
+         "Enter Password" =>  "Entrer le mot de passe",
+         "Email" => "E-mail",
+         "FILL IN ALL THE FIELDS" => "Remplissez tous les champs",
+         "FORGOTTEN PASSWORD" => "mot de passe oublié",
+         "REGISTER A NEW ROBOT" => "enregistrer un nouveau robot",
+         "REGISTER A NEW USER" => "enregistrer un nouvel utilisateur",
+         "FILL OUT THIS FIELD" => "remplissez ce champ",
+
+     );              
+//register a robot//
+
+     $lang = array (
+         "Register Robot" => "enregistrer un robot",
+         "Robot name" => "nom du robot" ,
+         "Serial Number" =>  "numéro de série",
+         "Team name" => "Nom de l'équipe",
+         "Password" => "le mot de passe",
+         "Confirm pasword" =>  "Confirmez le mot de passe",
+         "Create account" => "créer un compte",
+
+     );              
+ 
+    $lang = array (
+
+        "Register User" => "enregistrer l'utilisateur",
+        "Username" => "nom d'utilisateur" ,
+        "First name" => "Prénom",
+        "Last name" => "nom de famille",
+        "Date of Birth" => "date de naissance",
+        "Email" => "E-mail",
+        "Register" => "S'inscrire" ,
+
+    );
+?> 
---
+++ Lanuage translations/It.php
@@ -0,0 +1,90 @@
+<?php
+//Home page translation//
+//header//
+    $lang = array (
+        "MENUBAR" => "BARRA DEI MENU",
+        "HOME" => "HOME",
+        "EVENTS" => "EVENTI",
+        "LEADERBOARD" => "CLASSIFICA",
+        "PROJECT BATTLEBOTS" => "PROGETTO DEL BATTLEBOTS",
+        "WELCOME TO PROJECT BATTLEBOTS!" =>  "BENVENUTI NEL PROGETTO BATTLEBOTS!" 
+        "LOGIN" => "ACCEDI",
+
+    );
+
+//Footer//
+
+    $lang = array (
+         "CONTACT US!" => "CONTATTICI!",
+
+     );   
+
+//Events page//
+        
+    $lang = array (
+         "EVENTS" => "EVENTI!",
+         "There are no availave events" => "Non ci sono eventi disponibili",
+
+     );   
+        
+//Leaderboard page//
+  
+    $lang = array (
+         "LEADERBOARD" => "CLASSIFICA",
+         "POSITION" => "POSIZIONE",
+         "ROBOTNAME" => "NOME DEL ROBOT",
+         "POINTS" => "PUNTI",
+
+     );   
+       
+//Mini games page//
+
+    $lang = array (
+         "SCORES" => "PUNTEGGI",
+         "RULES" => "REGOLE",
+
+     );  
+
+//bets and login page//
+ 
+    $lang = array (
+         "LOGIN" => "ACCEDI",
+         "I HAVE A ROBOT" => "HO UN ROBOT",
+         "I AM A VIEWER" => "SONO UN SPETTATORE",
+         "Serial no" =>  "Numero seriale",
+         "Enter Password" =>  "Inserire la password",
+         "Email" => "E-mail",
+         "FILL IN ALL THE FIELDS" => "COMPILA TUTTI I CAMPI",
+         "FORGOTTEN PASSWORD" => "PASSWORD DIMENTICATA",
+         "REGISTER A NEW ROBOT" => "REGISTRA UN ROBOT NUOVO",
+         "REGISTER A NEW USER" => "REGISTERA UN NUOVO UTENTE",
+         "FILL OUT THIS FIELD" => "COMPILA QUESTO CAMPO",
+
+     );              
+//register a robot//
+
+     $lang = array (
+         "Register Robot" => "Registra Robot",
+         "Robot name" => "Nome del Robot" ,
+         "Serial Number" =>  "Numero seriale",
+         "Team name" => "Nome del squadra",
+         "Password" => "Password",
+         "Confirm pasword" =>  "Conferlma il Password",
+         "Create account" => "Creare un profilo",
+
+     );              
+ 
+    $lang = array (
+
+        "Register User" => "Registra un utente",
+        "Username" => "Nome utente" ,
+        "First name" => "Nome",
+        "Last name" => "Cognome" ,
+        "Date of Birth" => "Data di nascita",
+        "Email" => "E-mail",
+        "Register" => "Registra" ,
+
+    );
+    
+
+?> 
