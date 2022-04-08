1592bcfc04de4fe6a3116a54fba7375d6879152b
TerryIoannou <91316685+TerryIoannou@users.noreply.github.com>
2022-04-08 15:37:28 +0200

    Update SideBar.php

--- SideBar.php
+++ SideBar.php
@@ -4,19 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
     <title>Admin Panel</title>
 </head>
 <body class="backgroundsidebar">
     <div class="Sidebar">
     <div>
-    <img src="Jarvis.gif" class="jarvis">
+    <img src="uploads/admin_contact_photos/Jarvis.gif" class="jarvis">
     </div>
-    <a href="Admin.php" clas>Admin Home</a>
+    <a href="Admin.php" clas>Admin Home</a> 
     <br>
     <a href="Userpaneladmin.php">User Panel</a>
     <br>
-    <a href="Index.php">Live stream</a>
+    <a href="liveEvent.php">Live stream</a>
     <br>
     <a href="Robotpanel.php">Robot Panel</a>
     <br>
@@ -25,8 +25,9 @@
     <a href="feedback_overview.php">FeedBack</a>
     <br>
     <a href="contact_admin_overview.php">Contact Panel</a>
+    <br>
+    <a href="leaderboardpanel.php">LeaderBoard</a>
     </div>
     
 </body>
 </html>
->>>>>>> c9b8c9a433916873af303092d9af82b4b5b4dadf
